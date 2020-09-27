0810ea6860ea4f1c9209df82c5641a8848cb1c28
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-27 16:47:13 +0530

    Added: Ability to validate last name

--- main.js
+++ main.js
@@ -2,15 +2,22 @@ const Result = require('./UserRegistration')
 var readline = require('readline');
 console.log("Welcome to user validation Program")
 console.log("You have following Choices:")
-console.log("1.First Name ");
+console.log("1.First Name 2.Last Name ");
 var input = readline.createInterface(process.stdin, process.stdout);
 input.question("Enter your choice:", function (Choice) {
 
     switch (Choice) {
         case "1":
-            input.question("Enter First Name: ", function (firstName) {
-                Result.validateFirstName(firstName);
+            input.question("Enter First Name: ", function (namePattern) {
+                Result.validateName(namePattern);
                 process.exit();
             })
+            break;
+        case "2":
+            input.question("Enter Last Name: ", function (namePattern) {
+                Result.validateName(namePattern);
+                process.exit();
+            })
+            break;
     }
 })
--- userRegistration.js
+++ userRegistration.js
@@ -1,7 +1,7 @@
 class UserRegistration {
-    validateFirstName = (Name) => {
-        let firstName = /^[A-Z]{1}([a-z]{2,15})$/.test(Name)
-        console.log(firstName);
+    validateName = (Name) => {
+        let namePattern = /^[A-Z]{1}([a-z]{2,15})$/.test(Name)
+        console.log(namePattern);
     }
 }
 module.exports = new UserRegistration()
