7255c2946a5ecaf1d7c85c13bc2e7e101c5c68db
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-27 18:06:13 +0530

    Refactored: Code with proper documentation and readability

--- main.js
+++ main.js
@@ -8,32 +8,36 @@ input.question("Enter your choice:", function (Choice) {
 
     switch (Choice) {
         case "1":
+            console.log("NOTE: First name should start with upperCase and should have minimum three chracters")
             input.question("Enter First Name: ", function (namePattern) {
                 Result.validateName(namePattern);
                 process.exit();
             })
             break;
         case "2":
+            console.log("NOTE: Last name should start with upperCase and should have minimum three chracters")
             input.question("Enter Last Name: ", function (namePattern) {
                 Result.validateName(namePattern);
                 process.exit();
             })
             break;
-            case "3":
-                console.log("Validate email Id should be like following samples: "+
+        case "3":
+            console.log("NOTE: Validate email Id should be like following samples: " +
                 "abc@yahoo.com, abc-100@yahoo.com, abc.100@yahoo.com, abc111@abc.com, abc-100@abc.net, abc.100@abc.com.au, abc@1.com, abc@gmail.com.com")
             input.question("Enter Email Id: ", function (emailPattern) {
                 Result.validateEmail(emailPattern);
                 process.exit();
             })
             break;
-            case "4":
+        case "4":
+            console.log("NOTE: Phone number should start with 2 digit country code followed by space and 10 digit number")
             input.question("Enter Phone Number: ", function (phoneNoPattern) {
                 Result.validatePhoneNo(phoneNoPattern);
                 process.exit();
             })
             break;
-            case "5":
+        case "5":
+            console.log("NOTE: Password should be of minimum 8 chars including 1 upperCase,1 number,1 special char")
             input.question("Enter Password: ", function (passwordPattern) {
                 Result.validatePassword(passwordPattern);
                 process.exit();
--- userRegistration.js
+++ userRegistration.js
@@ -1,16 +1,28 @@
 class UserRegistration {
+    /**
+         * Function to validate first name and last name
+         */
     validateName = (Name) => {
         let namePattern = /^[A-Z]{1}([a-z]{2,15})$/.test(Name)
         console.log(namePattern);
     }
+    /**
+         * Function to validate emailId
+         */
     validateEmail = (EmailId) => {
         let emailPattern = /^[0-9a-zA-Z]+([+_.-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){1,2}$/.test(EmailId)
         console.log(emailPattern);
     }
+    /**
+         * Function to validate phone number
+         */
     validatePhoneNo = (PhoneNo) => {
         let phoneNoPattern = /^[0-9]{2}[ ][0-9]{10}$/.test(PhoneNo)
         console.log(phoneNoPattern);
     }
+    /**
+         * Function to validate password
+         */
     validatePassword = (PassWord) => {
         let passwordPattern = /(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%+!]{1}).{8,}/.test(PassWord)
         console.log(passwordPattern);
