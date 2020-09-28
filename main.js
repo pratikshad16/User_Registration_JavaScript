56b64bf8bb27f2258bd987d644a8d04778e289ef
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-28 12:14:51 +0530

    Refactored: Code using arrow functions

--- main.js
+++ main.js
@@ -4,41 +4,35 @@ console.log("Welcome to user validation Program")
 console.log("You have following Choices:")
 console.log("1.First Name 2.Last Name 3. Email Id 4.Phone Number 5.Password");
 var input = readline.createInterface(process.stdin, process.stdout);
-input.question("Enter your choice:", function (Choice) {
+input.question("Enter your choice:",  (Choice)=> {
 
     switch (Choice) {
         case "1":
-            console.log("NOTE: First name should start with upperCase and should have minimum three chracters")
-            input.question("Enter First Name: ", function (namePattern) {
+            input.question("Enter First Name: ", (namePattern)=> {
                 Result.validateName(namePattern);
                 process.exit();
             })
             break;
         case "2":
-            console.log("NOTE: Last name should start with upperCase and should have minimum three chracters")
-            input.question("Enter Last Name: ", function (namePattern) {
+            input.question("Enter Last Name: ",  (namePattern)=> {
                 Result.validateName(namePattern);
                 process.exit();
             })
             break;
         case "3":
-            console.log("NOTE: Validate email Id should be like following samples: " +
-                "abc@yahoo.com, abc-100@yahoo.com, abc.100@yahoo.com, abc111@abc.com, abc-100@abc.net, abc.100@abc.com.au, abc@1.com, abc@gmail.com.com")
-            input.question("Enter Email Id: ", function (emailPattern) {
+            input.question("Enter Email Id: ",  (emailPattern)=> {
                 Result.validateEmail(emailPattern);
                 process.exit();
             })
             break;
         case "4":
-            console.log("NOTE: Phone number should start with 2 digit country code followed by space and 10 digit number")
-            input.question("Enter Phone Number: ", function (phoneNoPattern) {
+            input.question("Enter Phone Number: ",  (phoneNoPattern)=> {
                 Result.validatePhoneNo(phoneNoPattern);
                 process.exit();
             })
             break;
         case "5":
-            console.log("NOTE: Password should be of minimum 8 chars including 1 upperCase,1 number,1 special char")
-            input.question("Enter Password: ", function (passwordPattern) {
+            input.question("Enter Password: ", (passwordPattern)=> {
                 Result.validatePassword(passwordPattern);
                 process.exit();
             })
